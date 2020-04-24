44cbbb860f5480dc5f98224fdac7a2a8a69c2f7c
Andrey Mukamolov <fobo66@protonmail.com>
2020-04-24 20:27:42 +0300

    Initial commit

---
+++ app/src/main/java/io/github/fobo66/demoscene/DemoSurfaceView.kt
@@ -0,0 +1,21 @@
+package io.github.fobo66.demoscene
+
+import android.content.Context
+import android.opengl.GLSurfaceView
+
+class DemoSurfaceView(context: Context): GLSurfaceView(context) {
+
+    private val renderer: DemoGLRenderer
+
+    init {
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2)
+
+        renderer = DemoGLRenderer()
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(renderer)
+    }
+
+}
---
+++ app/src/main/java/io/github/fobo66/demoscene/FullscreenActivity.kt
@@ -0,0 +1,40 @@
+package io.github.fobo66.demoscene
+
+import androidx.appcompat.app.AppCompatActivity
+import android.opengl.GLSurfaceView
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class FullscreenActivity : AppCompatActivity() {
+
+    private lateinit var glSurfaceView: GLSurfaceView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        glSurfaceView = DemoSurfaceView(this)
+
+        setContentView(glSurfaceView)
+        glSurfaceView.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+    }
+
+    override fun onStart() {
+        super.onStart()
+        glSurfaceView.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        glSurfaceView.onPause()
+    }
+}
