9f83d97fa439a0a7c9fb23f29e36ce999f69a509
Andrey Mukamolov <fobo66@protonmail.com>
2020-04-28 14:25:44 +0300

    Cleanup

--- app/src/main/java/io/github/fobo66/demoscene/DemoActivity.kt
+++ app/src/main/java/io/github/fobo66/demoscene/DemoActivity.kt
@@ -10,14 +10,14 @@ import android.view.WindowManager
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class FullscreenActivity : AppCompatActivity() {
+class DemoActivity : AppCompatActivity() {
 
     private lateinit var glSurfaceView: GLSurfaceView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
